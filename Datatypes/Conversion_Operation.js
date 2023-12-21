dfba09fddba1b0add6bf094bdb49c99b89769304
imshubham07 <mrshubhamdubey20@gmail.com>
2023-12-22 00:48:35 +0530

    Conversion of Data Types

---
+++ Datatypes/Conversion_Operation.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0 => false 
+// "" => false
+//"Shubham" => true
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
