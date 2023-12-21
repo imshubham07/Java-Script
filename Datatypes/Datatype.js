40c2482afdd86f4aad75a35cb14a791f54a4bf2e
imshubham07 <mrshubhamdubey20@gmail.com>
2023-12-21 23:59:46 +0530

    Data Types of JavaScript

---
+++ Datatypes/Datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+console.log( 3
+    + 
+    3 )  // code readability Should be high
+
+
+console.log("Shubham");
+
+
+let name = "Shubham"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+
+
+// Number => 2 to power 53
+// bigint
+//String => ""//use prefer to double court you can use single court
+// boolean => true/false
+//null => standalone valuev
+// undefined => value not assigned
+//symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
