3b31ea1a691462ca39ebcf8212851b211f2a73f8
imshubham07 <mrshubhamdubey20@gmail.com>
2023-12-25 23:54:28 +0530

    Comparision of Data Types and Cheak Equality

---
+++ Datatypes/Comparision.js
@@ -0,0 +1,25 @@
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+
+
+//  console.log("2" > 1);
+// console.log("02" > 1);
+
+
+/*   line no 15 to 21 these type of conversion always avode
+ */
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  is called strict cheak
+
+console.log("2" === 2);
