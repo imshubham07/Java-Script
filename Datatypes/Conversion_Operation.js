b97addc756f7f08865911320e7b688856ba9ca10
imshubham07 <mrshubhamdubey20@gmail.com>
2023-12-25 23:25:07 +0530

    Conversion of Data types and Operation

--- Datatypes/Conversion_Operation.js
+++ Datatypes/Conversion_Operation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "1"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1 => true; 0 => false 
 // "" => false
 //"Shubham" => true
@@ -23,5 +23,46 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// ******************************************** Operation ***********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// // (**) Use for Power 
+// console.log(2**2); // two the power two
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Shubham";
+// concatenate of two strings 
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(2 + 3 + 5 * 2 % 5);// This type of conversion is correct only in test but it is not the correct method.
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCharge = 100;
+++gameCharge;
+console.log(gameCharge);
+
+
+
