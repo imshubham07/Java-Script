85d16a4976d324694c8801a38959263e22ab4187
imshubham07 <imshubham07>
2024-01-07 21:49:28 +0530

    String and its function in javaScript

--- Strings/String.js
+++ Strings/String.js
@@ -5,6 +5,7 @@
 // // console.log(name + repoCount + " value");
 
 // // The new method is string manipulation and we create placeholders and can directly inject whatever value they have.
+//always use this method 
 // console.log(`hello my name is  ${name} and my repoCount is ${repoCount}`);//${} use for writen value of string in console log
 
 //string Declearation another type
@@ -17,16 +18,32 @@ const gameName = new String('shubham-kumar-dubey')
 // // to cheak how many element in a String
 // console.log(gameName.length);
 // using prototype function 
-console.log(gameName.toUpperCase(gameName));
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('u'));
+// console.log(gameName.toUpperCase(gameName));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
 
 
-const newString = gameName.substring(0,4);
-console.log(newString);
+// const newString = gameName.substring(0,4);
+// console.log(newString);
 
 
 // in Slice function you can give negative value
-const anotherString = gameName.slice(-4,-8)
-console.log(anotherString);
- 
+// const anotherString = gameName.slice(-18,7)
+// console.log(anotherString);
+
+
+// const newStringOne = "  Shubham   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //trim is use for remove extra spaces
+
+
+// When you want to add space in the URL, it appears as (%20), use replace for that.
+const url = "https://shubham.com/shubham%20dubey"
+
+const web = url.replace('%20', '-')
+
+console.log(web);
+
+console.log(url.includes('shubham'));//include use for serach keyword in url
+
+console.log(gameName.split('-'));//split use for split a word based on array
