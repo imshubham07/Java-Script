1f4b9536bf215391b31b4c69e037cf52f40d6b69
imshubham07 <imshubham07>
2024-01-12 23:31:41 +0530

    Array Basic 2

---
+++ Arrays/Array_basic2.js
@@ -0,0 +1,7 @@
+const marval_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Batman", "flash"]
+
+ marval_heros.push(dc_heros)
+ //This shows that in JavaScript there is an array within an array.
+ console.log(marval_heros);
+ console.log(marval_heros[3][1]);
