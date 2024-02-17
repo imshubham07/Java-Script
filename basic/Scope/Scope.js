0a8884e26b1bc67d4918b99b226177cc131c3540
imshubham07 <mrshubhamdubey20@gmail.com>
2024-02-18 00:02:07 +0530

    Global and Local Scoping

--- basic/Scope/Scope.js
+++ basic/Scope/Scope.js
@@ -1,19 +1,18 @@
-// var c = 300
+
+var c = 300  
 
 let a = 300
+
 if(true){
     let a = 10 
     const b = 20
-    // var c = 30
-    // console.log("INNER: ", a);
+    var c = 30
+    console.log("INNER: ", a);
 
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
+
 
 // console.log(a);
 // console.log(b);
-// console.log(c);
+console.log(c);
