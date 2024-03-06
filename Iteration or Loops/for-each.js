175057533766221482056b570b1d73529059f28d
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-06 20:02:33 +0530

    For Each Loops in JavaScript

---
+++ Iteration or Loops/for-each.js
@@ -0,0 +1,42 @@
+const coding = ["js", "ruby", "java","pyton","cpp"]
+
+// coding.forEach( function  (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach((value)=> {
+//     console.log(value);
+// })
+
+// function  printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(index, item , arr);
+// })
+
+const myCoding = [
+    {
+        languageName:"JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName:"C++",
+        languageFileName: "CPP"
+    },
+    {
+        languageName:"Java",
+        languageFileName: "java"
+    },
+    {
+        languageName:"python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+
+    console.log(item.languageName);
+} )
